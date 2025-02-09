33aca2a04ccf32f5fe232f1c6d94172b4e5f5f27
PyoMyeongchan <vyaudcks@gmail.com>
2025-02-10 08:30:40 +0900

    Read me

--- L20250210/obj/Debug/net8.0/L20250210.AssemblyInfo.cs
+++ L20250210/obj/Debug/net8.0/L20250210.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("L20250210")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a0dd562956744e58bc93b00a49719b43e4d5b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499911f3f1ca50665b9c6f0facad215b201be259")]
 [assembly: System.Reflection.AssemblyProductAttribute("L20250210")]
 [assembly: System.Reflection.AssemblyTitleAttribute("L20250210")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
