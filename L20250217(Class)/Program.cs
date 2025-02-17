e5d39f04a46007f06130045f87b3ce265bdaffa8
PyoMyeongchan <vyaudcks@gmail.com>
2025-02-17 16:44:05 +0900

    class / 다형성 /  as / is

---
+++ L20250217(Class)/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace L20250217_Class_
+{
+    //공통기능은 부모에게, 특화기능은 자식에게
+
+    class Monster
+    {
+        public virtual void Move()
+        {
+            Console.WriteLine("이동한다");
+        }
+
+    }
+
+    class Slime : Monster
+    {
+
+        public override void Move()
+        {
+            Console.WriteLine("미끄러진다");
+
+        }
+
+        public void Sticky()
+        {
+
+            Console.WriteLine("끈적인다");
+        }
+    }
+
+    class Goblin : Monster
+    {
+
+        public override void Move()
+        {
+            Console.WriteLine("뛰어다닌다");
+
+        }
+
+        public void Throw()
+        {
+            Console.WriteLine("던진다");
+
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster[] monster = new Monster[3];
+            monster[0] = new Slime();
+            monster[1] = new Goblin();
+            monster[2] = new Slime();
+
+            //Down casting, 동적변환
+            for (int i = 0; i < monster.Length; i++)
+            {
+                Slime s = monster[i] as Slime;
+                if (s != null)
+                {
+
+                    s.Sticky();
+
+                }
+
+                Goblin g = monster[i] as Goblin;
+                if (g!= null)
+                {
+
+                    g.Throw();
+
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ L20250217/Monster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20250217
+{
+    public class Monster : GameObject
+    {
+        private Random rand = new Random();
+
+        public Monster(int inX, int inY, char inShape)
+        {
+            X = inX;
+            Y = inY;
+            Shape = inShape;
+        }
+
+
+        //몬스터의 AI와 같다. 게임AI와 머신러닝AI(ChatGPT)는 완전 다르다.
+        public override void Update()
+        {
+            //33ms 사운드 입력처리 효과 UI AI(미리 만들어 놓은 로직, 패턴)
+            //시야(RayCasting)
+            //사운드
+            //기획 / 패턴이 들어간다.
+            int Direction = rand.Next(0,4);
+
+            if (Direction == 0)
+            {
+                Y--;
+            }
+            if (Direction == 1)
+            {
+                Y++;
+            }
+            if (Direction == 2)
+            {
+                X--;
+            }
+            if (Direction == 3)
+            {
+                X++;
+            }
+        }
+
+
+    }
+}
