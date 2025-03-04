8697b8ba8adf6e0560ebdbc0dfbf7c533a27e9d9
PyoMyeongchan <vyaudcks@gmail.com>
2025-03-04 14:37:31 +0900

    SDL

---
+++ L20250217-main/Input.cs
@@ -0,0 +1,42 @@
+﻿using SDL2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L20250217
+{
+    public class Input
+    {
+        public Input() 
+        {
+
+        }
+
+        static public void Process()
+        {
+            //if (Console.KeyAvailable)
+            //{
+            //    keyInfo = Console.ReadKey(true);
+            //}
+        }
+
+        static protected ConsoleKeyInfo keyInfo;
+
+        static public bool GetKeyDown(ConsoleKey key)
+        {
+            return (keyInfo.Key == key);
+        }
+
+        static public bool GetKeyDown(SDL.SDL_Keycode key)
+        {
+            return (Engine.Instance.myEvent.key.keysym.sym == key);
+        }
+
+        public static void ClearInput()
+        {
+            keyInfo = new ConsoleKeyInfo();
+        }
+    }
+}
