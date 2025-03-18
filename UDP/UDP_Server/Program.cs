9bb75202c3fe4863591fa568605e695796f48c43
PyoMyeongchan <vyaudcks@gmail.com>
2025-03-18 14:44:36 +0900

    UDP

---
+++ UDP/UDP_Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 6000);
+
+            serverSocket.Bind(serverEndPoint);
+
+            byte[] buffer = new byte[1024]; // UDP는 다 받아야한다.
+            EndPoint clientEndPoint = (EndPoint)serverEndPoint;
+
+            // 블락킹
+            int recvLegth = serverSocket.ReceiveFrom(buffer, ref clientEndPoint);
+
+            int sendLegth = serverSocket.SendTo(buffer, clientEndPoint);
+
+            serverSocket.Close();
+
+        }
+    }
+}
